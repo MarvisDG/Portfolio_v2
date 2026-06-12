8baa6c51c4fb0d4c2bc405f4b89e3c40c0e4fe16
Umohmarvelous <umohmarvelous@gmail.com>
2026-06-12 02:51:20 +0100

    changes css file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "aos/dist/aos.css";
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
